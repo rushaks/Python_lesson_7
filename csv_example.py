e41dd8b7e7599eb551cd94ec09ae00d45dbe7524
bashlaevs@ya.ru <bashlaevs@ya.ru>
2020-07-30 19:31:34 +0300

    init

--- csv_example.py
+++ csv_example.py
@@ -1,8 +1,40 @@
 import csv
 
 # Записываем данные в файл
-car_data = [['brand','price','year'],['Volvo',1.5,2007],['Lada',1.2,2010]]
-with open('example.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(car_data)
-print('Writing complete')
+# car_data = [['brand', 'price', 'year'],['Volvo', 1.5, 2017],['Lada', 0.5, 2018],['Audi', 2.0, 2018]]
+# with open('example.csv', 'w') as f:
+#     writer = csv.writer(f)
+#     writer.writerows(car_data)
+# print('Writing complete')
+
+# Читаем файлы
+# with open('example.csv', 'r') as f:
+#     reader = csv.reader(f)
+#     for row in reader:
+#         print(row)
+
+# DictWriter
+# data_dict = [{'Name': 'Dima', 'age': 28},
+#              { 'age': 29, 'Name': 'Kate'},
+#              {'Name': 'Mike', 'age': 31}]
+#
+# fieldnames = ['Name','age']
+#
+# with open('example_1.csv','w') as f:
+#     writer = csv.DictWriter(f, fieldnames = fieldnames)
+#     writer.writeheader()
+#     for i in range(len(data_dict)):
+#         writer.writerow(data_dict[i])
+
+# DictReader
+# with open('example_1.csv') as f:
+#     reader = csv.DictReader(f)
+#     for row in reader:
+#         #print(row)
+#         print(dict(row))
+
+import pandas as pd
+
+DataFrame_from_csv = pd.read_csv('example_1.csv')
+print(type(DataFrame_from_csv))
+print(DataFrame_from_csv)
